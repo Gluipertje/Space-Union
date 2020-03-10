674bde43636a82e560c99f352443d1f6dbd75d50
Gluipertje <makeitunrealpc@gmail.com>
2020-03-10 11:30:26 +0100

    fixed stamina bar

--- src/actors/PlayerController.gd
+++ src/actors/PlayerController.gd
@@ -5,12 +5,12 @@ var _speednew
 var _playerPos
 var _jumpCount = 0 # This portion declares all 'private' variables which cant be accessed by other scripts
 
-
+onready var GUI = get_node("Control")
 onready var camera = get_node( "Camera2D" )
-onready var FPSText = get_node("Camera2D/FPSText")
+onready var FPSText = get_node("Control/FPSText")
 onready var jetpackParticle = get_node( "Particles2D") # This portion gets some other nodes which are attached to the player
  
-var stamina: = 200.0
+var stamina: = 500.0
 var JPFuel = 500.0
 export var staminaDepletion: = 100.0
 export var staminaRegeneration: = 50.0
@@ -94,6 +94,7 @@ func move(_velocity, _direction):
 func checkWorldEnd():
 	_playerPos = get_position()
 	print(_playerPos)
+	GUI.set_position(_playerPos)
 	if _playerPos.x > global._realWorldSize.x:
 		set_position(global.coordinateStart)
 	elif _playerPos.x < global.coordinateStart.x:
